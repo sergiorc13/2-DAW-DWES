688a76b9f6ea9f6a115785f421f40f2fd460aecb
Sergio Rodríguez <124568482+sergiorc13@users.noreply.github.com>
2023-11-14 14:06:53 +0100

    Delete Prueba/Prueba directory

--- Prueba/Prueba/estructuraWeb/home.inc.php
+++
@@ -1,8 +0,0 @@
-
-    
-    <div class="botones-iniciar-registrar">
-        <a href="?ruta=login">Iniciar sesión</a>
-        <a href="?ruta=registro">Registrarse</a>
-    </div>
-    
-    <?php include_once "estructuraWeb/pie.inc.php"; ?>
--- Prueba/Prueba/estructuraWeb/pie.inc.php
+++
@@ -1,3 +0,0 @@
-<footer>
-    Desarrollado por Sergio Rodríguez Costa <br> &#169; 2023 - Todos los derechos reservados
-</footer>
